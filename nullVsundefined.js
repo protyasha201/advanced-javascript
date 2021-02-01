b90e6f60180db77bb9f0c090410197398151a036
protyasha201 <protyasha0999@gmail.com>
2021-02-01 23:24:31 +0600

    addeded some files

---
+++ nullVsundefined.js
@@ -0,0 +1,19 @@
+// let herName;
+
+// function add(num1, num2){
+//     console.log(num1 + num2);      //this will show undefined.
+// }
+
+// function add(num1, num2){
+//     console.log(num2);  
+// }
+
+//const result = add(82);   //sending one argument instead of two will give undefined.
+
+//const something = {nameIs: "Protyasha" , phone:3413242};  //if i try to get a gfName that is not included in the something object then it will return a undefined if console.log().
+
+let fun = undefined;
+// console.log(fun);
+
+let ages = [12,34,3,1];
+console.log(ages[10]);
